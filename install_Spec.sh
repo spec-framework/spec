dac11da4a61df5f19cc07fae9a4fda9e98b3078c
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-01-23 22:38:41 -0300

    try to fix load

--- install_Spec.sh
+++ install_Spec.sh
@@ -6,14 +6,24 @@ fi
 
 rm -rf pharo-vm
 
+curl -o prescript.st -L https://raw.github.com/SpecForPharo/spec/master/preload.st
 curl -o script.st -L https://raw.github.com/SpecForPharo/spec/master/installScript.st
+curl -o postscript.st -L https://raw.github.com/SpecForPharo/spec/master/postload.st
 curl -o spec.zip -L https://github.com/SpecForPharo/spec/archive/master.zip
-unzip -uo spec.zip
+unzip -u -o -q spec.zip
 
 curl get.pharo.org/vm | bash
 
 IMAGE=$PWD/$1
-SCRIPT=`cat script.st`
 
-echo "Loading Spec code into $IMAGE"
+SCRIPT=`cat preload.st`
+echo "Preloading"
+./pharo "$IMAGE" eval --save "$SCRIPT"
+
+SCRIPT=`cat script.st`
+echo "Loading Spec code"
+./pharo "$IMAGE" eval --save "$SCRIPT"
+
+SCRIPT=`cat postload.st`
+echo "Loading Spec code"
 ./pharo "$IMAGE" eval --save "$SCRIPT"
